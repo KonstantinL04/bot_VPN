1b153e44ce9736986d3387e28fd29afaf2e87c29
Litvinov Kosntantin <litaistopet@gmail.com>
2024-06-28 15:02:18 +0800

    Поправил некоторые вещи, связанные с токеном

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 import asyncio
-
 from aiogram import Bot , Dispatcher
 from handlers.user_handlers import router as user_router
 from handlers.other_handlers import router as other_router
-
-
 import logging
-
-from config import TOKEN
+from config import Config, load_config
 from handlers.user_handlers import router
 
+config: Config = load_config()
+BOT_TOKEN: str = config.tg_bot.token
 
-bot = Bot(token=TOKEN)
+bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot=bot)
-
- 
 
 async def main():
     dp.include_router(user_router)
