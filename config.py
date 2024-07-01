9ea35a7a753c0ac083aeca4f9a48d66126f25fbd
Litvinov Kosntantin <litaistopet@gmail.com>
2024-07-01 16:32:27 +0800

    Добавлено: - Тестовая оплата подписок

--- config.py
+++ config.py
@@ -7,11 +7,15 @@ from environs import Env
 class TgBot:
     token: str  # Токен для доступа к телеграм-боту
 
-
+@dataclass
+class PayToken:
+    token: str
+@dataclass
+class PaysToken:
+    token: PayToken
 @dataclass
 class Config:
     tg_bot: TgBot
-
 
 # Создаем функцию, которая будет читать файл .env и возвращать экземпляр
 # класса Config с заполненными полями token и admin_ids
@@ -19,3 +23,8 @@ def load_config(path: str | None = None) -> Config:
     env = Env()
     env.read_env(path)
     return Config(tg_bot=TgBot(token=env('BOT_TOKEN')))
+
+def load_pay_token(path: str | None = None) -> PaysToken:
+    env = Env()
+    env.read_env(path)
+    return PaysToken(token=PayToken(token=env('PAYMENT_TOKEN')))
--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,11 +1,20 @@
-from aiogram import F, Router
+from aiogram import F, Router, types
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message, CallbackQuery, PreCheckoutQuery, ContentType
 import keyboards.keyboards as kb
 from lexicon.lexicon_ru import LEXICON_RU
 from services.services import get_user_name
+from config import PaysToken, load_pay_token
+
+payToken: PaysToken = load_pay_token()
+PAYMENT_TOKEN: str = payToken.token.token
 
 router = Router()
+
+# Prices
+PRICE_1_MONTH = types.LabeledPrice(label="Подписка на 1 месяц", amount=120 * 100)  # 120 руб в копейках
+PRICE_3_MONTHS = types.LabeledPrice(label="Подписка на 3 месяца", amount=320 * 100)  # 320 руб в копейках
+PRICE_6_MONTHS = types.LabeledPrice(label="Подписка на 6 месяцев", amount=600 * 100)  # 600 руб в копейках
 
 # Главное меню
 @router.message(CommandStart())
@@ -42,6 +51,81 @@ async def check_pay(callback: CallbackQuery):
 @router.callback_query(F.data == 'month_Назад')
 async def back_to_subscribe(callback: CallbackQuery):
     await callback.message.edit_text(LEXICON_RU['subscribe'], reply_markup=await kb.inline_subscribes(), parse_mode='HTML')
+
+@router.callback_query(F.data == 'month_Оплатить 120₽')
+async def buy_120(callback: CallbackQuery):
+    if PAYMENT_TOKEN.split(':')[1] == 'TEST':
+        await callback.message.answer("Тестовый платеж!!!")
+
+    await callback.message.answer_invoice(
+        title="Подписка на 1 месяц",
+        description="Активация подписки на бота на 1 месяц",
+        provider_token=PAYMENT_TOKEN,
+        currency="rub",
+        photo_url="https://i.pcmag.com/imagery/reviews/00JXUu9pun1kRsTQWb6Pvh8-7.fit_lim.size_1050x591.v1569476244.jpg",
+        photo_width=400,
+        photo_height=200,
+        photo_size=1200,
+        is_flexible=False,
+        prices=[PRICE_1_MONTH],
+        start_parameter="one-month-subscription",
+        payload="test-invoice-payload-120"
+    )
+
+@router.callback_query(F.data == 'month_Оплатить 320₽')
+async def buy_320(callback: CallbackQuery):
+    if PAYMENT_TOKEN.split(':')[1] == 'TEST':
+        await callback.message.answer("Тестовый платеж!!!")
+
+    await callback.message.answer_invoice(
+        title="Подписка на 3 месяца",
+        description="Активация подписки на бота на 3 месяца",
+        provider_token=PAYMENT_TOKEN,
+        currency="rub",
+        photo_url="https://i.pcmag.com/imagery/reviews/00JXUu9pun1kRsTQWb6Pvh8-7.fit_lim.size_1050x591.v1569476244.jpg",
+        photo_width=400,
+        photo_height=200,
+        photo_size=1200,
+        is_flexible=False,
+        prices=[PRICE_3_MONTHS],
+        start_parameter="three-month-subscription",
+        payload="test-invoice-payload-320"
+    )
+
+@router.callback_query(F.data == 'month_Оплатить 600₽')
+async def buy_600(callback: CallbackQuery):
+    if PAYMENT_TOKEN.split(':')[1] == 'TEST':
+        await callback.message.answer("Тестовый платеж!!!")
+
+    await callback.message.answer_invoice(
+        title="Подписка на 6 месяцев",
+        description="Активация подписки на бота на 6 месяцев",
+        provider_token=PAYMENT_TOKEN,
+        currency="rub",
+        photo_url="https://i.pcmag.com/imagery/reviews/00JXUu9pun1kRsTQWb6Pvh8-7.fit_lim.size_1050x591.v1569476244.jpg",
+        photo_width=400,
+        photo_height=200,
+        photo_size=1200,
+        is_flexible=False,
+        prices=[PRICE_6_MONTHS],
+        start_parameter="six-month-subscription",
+        payload="test-invoice-payload-600"
+    )
+
+#проверка оплаты
+@router.pre_checkout_query(lambda query: True)
+async def pre_checkout_query(pre_checkout_q: PreCheckoutQuery):
+    await pre_checkout_q.bot.answer_pre_checkout_query(pre_checkout_q.id, ok=True)
+
+
+@router.message(F.content_type == ContentType.SUCCESSFUL_PAYMENT)
+async def successful_payment(message: Message):
+    print("SUCCESSFUL PAYMENT:")
+    payment_info = message.successful_payment.to_python()
+    for k, v in payment_info.items():
+        print(f"{k} = {v}")
+
+    await message.answer(f"Платеж на сумму {message.successful_payment.total_amount // 100} {message.successful_payment.currency} прошел успешно!!!")
 
 # Меню выбора помощи
 @router.callback_query(F.data == 'help')
