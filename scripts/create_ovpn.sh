2f2fbd8a10c125326c485e5df373dd5abbe30c6c
Litvinov Kosntantin <litaistopet@gmail.com>
2024-07-04 14:46:37 +0800

    fix_opl4

--- scripts/create_ovpn.sh
+++ scripts/create_ovpn.sh
@@ -3,7 +3,7 @@ USERNAME=$1
 DAYS=$2
 
 cd /etc/openvpn/server/easy-rsa/
-sudo -s ./easyrsa --batch --days=$DAYS build-client-full $DAYS+$USERNAME nopass
+sudo -s ./easyrsa --batch --days=$DAYS build-client-full $DAYS$USERNAME nopass
 
 {
     sudo -s cat /etc/openvpn/server/client-common.txt
@@ -11,13 +11,13 @@ sudo -s ./easyrsa --batch --days=$DAYS build-client-full $DAYS+$USERNAME nopass
     sudo -s cat /etc/openvpn/server/easy-rsa/pki/ca.crt
     echo "</ca>"
     echo "<cert>"
-    sudo -s sed -ne '/BEGIN CERTIFICATE/,$ p' /etc/openvpn/server/easy-rsa/pki/issued/$DAYS+$USERNAME.crt
+    sudo -s sed -ne '/BEGIN CERTIFICATE/,$ p' /etc/openvpn/server/easy-rsa/pki/issued/$DAYS$USERNAME.crt
     echo "</cert>"
     echo "<key>"
-    sudo -s cat /etc/openvpn/server/easy-rsa/pki/private/$DAYS+$USERNAME.key
+    sudo -s cat /etc/openvpn/server/easy-rsa/pki/private/$DAYS$USERNAME.key
     echo "</key>"
     echo "<tls-crypt>"
     sudo -s sed -ne '/BEGIN OpenVPN Static key/,$ p' /etc/openvpn/server/tc.key
     echo "</tls-crypt>"
-} > /home/konstantin/$DAYS+$USERNAME.ovpn
+} > /home/konstantin/$DAYS$USERNAME.ovpn
 
