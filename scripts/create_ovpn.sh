3e0b724359bd70fb2051309d78ba0103a78a86f5
Litvinov Kosntantin <litaistopet@gmail.com>
2024-07-04 14:33:42 +0800

    Исправлен скрипт

--- scripts/create_ovpn.sh
+++ scripts/create_ovpn.sh
@@ -3,7 +3,7 @@ USERNAME=$1
 DAYS=$2
 
 cd /etc/openvpn/server/easy-rsa/
-sudo -s ./easyrsa --batch --days=$DAYS build-client-full $USERNAME nopass
+sudo -s ./easyrsa --batch --days=$DAYS build-client-full $DAYS+$USERNAME nopass
 
 {
     sudo -s cat /etc/openvpn/server/client-common.txt
